0aac0d305a05788c053a1950437ceb06cc17d1b0
Manuel Antoniow <manuelantoniow@icloud.com>
2022-06-12 16:59:36 -0300

    arrays y objetos

--- js/main.js
+++ js/main.js
@@ -11,17 +11,33 @@ function promedio(num){
     return(total / contador)
 }
 
-let nombre = prompt("Ingrese nombre del alumno")
-let salida  = nombre + " ingresado/a"
-alert(salida)
-console.log(salida)
-
-let nota = Number(prompt("Ingresar una nota o -1 para terminar"))
-while(nota != -1 ){
-    if ( nota >= 1 && nota <= 10 ){
-        alert("El promedio actual de " + nombre + " es " + promedio(nota))
-        nota = Number(prompt("Ingresar otra nota o -1 para terminar"))
-    }else{
-        nota = Number(prompt("Nota incompatible ingrese otra o -1 para terminar"))
+class Alumno {
+    constructor(nombre, promedio) {
+        this.nombre  = nombre.toUpperCase();
+        this.promedio  = parseFloat(promedio);
     }
 }
+
+const alumnos = []
+let entrada = prompt("Ingrese nombre del alumno o esc para terminar")
+while(entrada != "esc"){
+    let pro = 0
+    alert(entrada + " ingresado/a")
+    console.log(entrada + " ingresado/a")
+    let nota = Number(prompt("Ingresar una nota o -1 para terminar"))
+    while(nota != -1 ){
+        if ( nota >= 1 && nota <= 10 ){
+            pro = promedio(nota)
+            alert("El promedio actual de " + entrada + " es " + pro)
+            nota = Number(prompt("Ingresar otra nota o -1 para terminar"))
+        }else{
+            nota = Number(prompt("Nota incompatible ingrese otra o -1 para terminar"))
+        }
+    }
+    alumnos.push(new Alumno(entrada, pro))
+    entrada = prompt("Ingrese nombre del alumno o esc para terminar")
+    contador = 0
+    total = 0
+}
+
+console.log(alumnos)
