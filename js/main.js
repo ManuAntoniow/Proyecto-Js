55155e4a05130ee65e6e3fd2e51c90fd8cd46050
Manuel Antoniow <manuelantoniow@icloud.com>
2022-06-14 18:27:03 -0300

    registro de notas agreagdo al objeto alumno

--- js/main.js
+++ js/main.js
@@ -12,8 +12,9 @@ function promedio(num){
 }
 
 class Alumno {
-    constructor(nombre, promedio) {
+    constructor(nombre, notas, promedio) {
         this.nombre  = nombre.toUpperCase();
+        this.notas = notas
         this.promedio  = parseFloat(promedio);
     }
 }
@@ -22,10 +23,12 @@ const alumnos = []
 let entrada = prompt("Ingrese nombre del alumno o esc para terminar")
 while(entrada != "esc"){
     let pro = 0
+    let notas = []
     alert(entrada + " ingresado/a")
     console.log(entrada + " ingresado/a")
     let nota = Number(prompt("Ingresar una nota o -1 para terminar"))
     while(nota != -1 ){
+        notas.push(nota)
         if ( nota >= 1 && nota <= 10 ){
             pro = promedio(nota)
             alert("El promedio actual de " + entrada + " es " + pro)
@@ -34,10 +37,13 @@ while(entrada != "esc"){
             nota = Number(prompt("Nota incompatible ingrese otra o -1 para terminar"))
         }
     }
-    alumnos.push(new Alumno(entrada, pro))
+    alumnos.push(new Alumno(entrada, notas, pro))
     entrada = prompt("Ingrese nombre del alumno o esc para terminar")
     contador = 0
     total = 0
 }
 
 console.log(alumnos)
+
+const aprobados = alumnos.filter((el) => el.promedio > 7)
+console.log(aprobados)
