42d3c3b4db8adb57c8767b4a7695f24a567f826f
Manuel Antoniow <manuelantoniow@icloud.com>
2022-06-29 22:19:13 -0300

    storage y json

--- js/main.js
+++ js/main.js
@@ -17,6 +17,8 @@ function promedio(num){
     return(total / 3)
 }
 
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) }
+
 class Alumno {
     constructor(nombre, notas, promedio) {
         this.nombre  = nombre.toUpperCase()
@@ -25,12 +27,18 @@ class Alumno {
     }
 }
 
+const alumnos = []
+const almacenados = JSON.parse(localStorage.getItem("listaAlumnos"))
+console.log(almacenados)
+for (const objeto of almacenados) {
+    alumnos.push(new Alumno(objeto.nombre, objeto.notas, objeto.promedio))
+}
+
 let inputNombre = document.querySelector('#input-nombre')
 let inputNota1 = document.querySelector('#input-nota1')
 let inputNota2 = document.querySelector('#input-nota2')
 let inputNota3 = document.querySelector('#input-nota3')
 let btnEnviar = document.querySelector('#btn-enviar')
-const alumnos = []
 
 btnEnviar.addEventListener('click', (e) => {
     e.preventDefault()
@@ -42,50 +50,26 @@ btnEnviar.addEventListener('click', (e) => {
 const btn1 = document.querySelector('#boton1')
 const btn2 = document.querySelector('#boton2')
 
-const aprobados = alumnos.filter((el) => el.promedio >= 7)
-console.log(alumnos)
-console.log(aprobados)
-
 btn1.onclick = () => {
-    console.log("Alumnos")
+    console.log(alumnos)
     for (const Alumno of alumnos) {
         let contenedor = document.createElement("div")
         contenedor.innerHTML = `<h3> Nombre: ${Alumno.nombre}</h3>
                                 <p>  Notas: ${Alumno.notas}</p>
-                                <b> Promedio: ${Alumno.promedio}</b>`
+                                <p> Promedio: ${Alumno.promedio}</p>`
         document.body.appendChild(contenedor)
+        guardarLocal(Alumno.nombre, JSON.stringify(Alumno));
     }
+    guardarLocal("listaAlumnos", JSON.stringify(alumnos))
 }
 btn2.onclick = () => {
-    console.log("Aprobados")
+    const aprobados = alumnos.filter((el) => el.promedio >= 7)
+    console.log(aprobados)
     for (const Alumno of aprobados) {
         let contenedor = document.createElement("div")
         contenedor.innerHTML = `<h3> Nombre: ${Alumno.nombre}</h3>
                                 <p>  Notas: ${Alumno.notas}</p>
-                                <b> Promedio: ${Alumno.promedio}</b>`
+                                <p> Promedio: ${Alumno.promedio}</p>`
         document.body.appendChild(contenedor)
     }
 }
-
-// let entrada = prompt("Ingrese nombre del alumno o esc para terminar")
-// while(entrada != "esc"){
-//     let pro = 0
-//     let notas = []
-//     alert(entrada + " ingresado/a")
-//     console.log(entrada + " ingresado/a")
-//     let nota = Number(prompt("Ingresar una nota o -1 para terminar"))
-//     while(nota != -1 ){
-//         notas.push(nota)
-//         if ( nota >= 1 && nota <= 10 ){
-//             pro = promedio(nota)
-//             alert("El promedio actual de " + entrada + " es " + pro)
-//             nota = Number(prompt("Ingresar otra nota o -1 para terminar"))
-//         }else{
-//             nota = Number(prompt("Nota incompatible ingrese otra o -1 para terminar"))
-//         }
-//     }
-//     alumnos.push(new Alumno(entrada, notas, pro))
-//     entrada = prompt("Ingrese nombre del alumno o esc para terminar")
-//     contador = 0
-//     total = 0
-// }
