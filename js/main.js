25473a79276c6b54e75dea6d73cabef1e0350801
Manuel Antoniow <manuelantoniow@icloud.com>
2022-07-06 21:15:40 -0300

    ordenar js

--- js/main.js
+++ js/main.js
@@ -8,11 +8,55 @@ class Profesor {
     }
 }
 
+class Alumno {
+    constructor(nombre, notas, promedio) {
+        this.id = Math.random()
+        this.nombre  = nombre.toUpperCase()
+        this.notas = notas
+        this.promedio  = Math.round(promedio)
+    }
+}
+
+function promedio(num){
+    total = num[0] + num[1] + num[2] 
+    console.log(total)
+    return(total / 3)
+}
+
+function revisarNota(event) {
+    const input = event.target;
+    input.value <= 0 ? (input.value = 1) : null
+    input.value >= 11 ? (input.value = 10) : null
+}
+
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) }
+
 const profesores = []
 profesores.push(new Profesor("Andres", "profe1", materias=["matematica", "scnat"])) 
 profesores.push(new Profesor("Julio", "profe2", materias=["", "scsoc"])) 
-let btnConfirmar = document.querySelector('#btn-confirmar')
 console.log(profesores)
+const alumnos = []
+const almacenados = JSON.parse(localStorage.getItem("listaAlumnos"))
+if (almacenados != null) {
+    console.log(almacenados)
+    for (const objeto of almacenados) {
+        alumnos.push(new Alumno(objeto.nombre, objeto.notas, objeto.promedio))
+    }
+}
+
+let btnConfirmar = document.querySelector('#btn-confirmar')
+let inputNombre = document.querySelector('#input-nombre')
+let inputNota1 = document.querySelector('#input-nota1')
+let inputNota2 = document.querySelector('#input-nota2')
+let inputNota3 = document.querySelector('#input-nota3')
+let btnEnviar = document.querySelector('#btn-enviar')
+const btn1 = document.querySelector('#boton1')
+const btn2 = document.querySelector('#boton2')
+const listaContenedor = document.querySelector('.listaContenedor')
+
+inputNota1.addEventListener('change', revisarNota)
+inputNota2.addEventListener('change', revisarNota)
+inputNota3.addEventListener('change', revisarNota)
 
 btnConfirmar.addEventListener('click', (e) => {
     e.preventDefault()
@@ -56,55 +100,6 @@ btnConfirmar.addEventListener('click', (e) => {
     }
 })
 
-function promedio(num){
-    total = num[0] + num[1] + num[2] 
-    console.log(total)
-    return(total / 3)
-}
-
-const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) }
-
-class Alumno {
-    constructor(nombre, notas, promedio) {
-        this.id = Math.random()
-        this.nombre  = nombre.toUpperCase()
-        this.notas = notas
-        this.promedio  = Math.round(promedio)
-    }
-}
-
-const alumnos = []
-const almacenados = JSON.parse(localStorage.getItem("listaAlumnos"))
-if (almacenados != null) {
-    console.log(almacenados)
-    for (const objeto of almacenados) {
-        alumnos.push(new Alumno(objeto.nombre, objeto.notas, objeto.promedio))
-    }
-}
-
-let inputNombre = document.querySelector('#input-nombre')
-let inputNota1 = document.querySelector('#input-nota1')
-let inputNota2 = document.querySelector('#input-nota2')
-let inputNota3 = document.querySelector('#input-nota3')
-let btnEnviar = document.querySelector('#btn-enviar')
-
-btnEnviar.addEventListener('click', (e) => {
-    e.preventDefault()
-    let notas = [Number(inputNota1.value), Number(inputNota2.value), Number(inputNota3.value)]
-    let pro = promedio(notas)
-    alumnos.push(new Alumno(inputNombre.value, notas, pro))
-    swal({
-        title: 'GENIAL',
-        text: 'Datos de alumno almacenado correctamente',
-        icon: 'success',
-        button: 'Siguiente'
-    })
-})
-
-const btn1 = document.querySelector('#boton1')
-const btn2 = document.querySelector('#boton2')
-const listaContenedor = document.querySelector('.listaContenedor')
-
 btn1.onclick = () => {
     console.log(alumnos)
     const datosAnteriores = document.getElementById("eliminar")
@@ -145,13 +140,3 @@ btn2.onclick = () => {
         document.getElementById(Alumno.id).style.color = "green"
     }
 }
-
-function revisarNota(event) {
-    const input = event.target;
-    input.value <= 0 ? (input.value = 1) : null
-    input.value >= 11 ? (input.value = 10) : null
-}
-
-inputNota1.addEventListener('change', revisarNota)
-inputNota2.addEventListener('change', revisarNota)
-inputNota3.addEventListener('change', revisarNota)
